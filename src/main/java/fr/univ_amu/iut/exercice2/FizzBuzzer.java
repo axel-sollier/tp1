bafd54e4ed4827918aae679f33ba5dc3f2de569a
axel-sollier <axel.sollier@etu.univ-amu.fr>
2017-05-18 15:13:11 +0200

    Tp1 Exercice 2

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,10 +3,21 @@ package fr.univ_amu.iut.exercice2;
 public class FizzBuzzer {
 
     public String computeString(int i) {
-        throw new RuntimeException("Not yet implemented !");
+
+        String s = String.valueOf(i);
+        if (i%3 == 0)
+            s = "Fizz";
+        else if (i%5 == 0)
+            s = "Buzz";
+        if (i%3 == 0 && i%5 == 0)
+            s = "FizzBuzz";
+        return s;
     }
 
     public String[] computeList(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        String[] src = new String[i];
+        for ( int x = 1; x<=i;++x) src[x-1] = computeString(x);
+        return src;
+
     }
 }
