77567939a7b1fbf65e019e08797c1c4521a55e65
axel-sollier <axel.sollier@etu.univ-amu.fr>
2017-05-18 17:16:38 +0200

    Tp1 Exercice 3

--- src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
+++ src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
@@ -1,7 +1,37 @@
 package fr.univ_amu.iut.exercice3;
 
 public class ConvertisseurDeNombreRomain {
-    public int enNombreArabe(String i) {
-        throw new RuntimeException("Not yet implemented !");
+    public int enNombreArabe(String i)    {
+        int valeur = 0;
+        int valeurChiffrePrecedent = Integer.MAX_VALUE;
+        for (char chiffre : i.toCharArray()){
+            int valeurChiffreCourant = valeurUnite(chiffre);
+            if(valeurChiffrePrecedent < valeurChiffreCourant)
+                valeur=valeur+valeurChiffreCourant-valeurChiffrePrecedent*2;
+            else
+                valeur += valeurUnite(chiffre);
+            valeurChiffrePrecedent=valeurChiffreCourant;
+        }
+        return valeur;
+    }
+    private int valeurUnite(char chiffre){
+        switch(chiffre){
+            case 'I' :
+                return 1;
+            case  'V' :
+                return 5;
+            case 'X' :
+                return 10;
+            case 'L':
+                return  50;
+            case 'C' :
+                return 100;
+            case 'D' :
+                return 500;
+            case 'M' :
+                return 1000;
+            default:
+                return 0;
+        }
     }
 }
